2496622b0122ed8a2dd76af4cc4ed8247ba8dbd0
Jason Esquivel <jesquive@gmu.edu>
2016-09-11 15:45:11 -0400

    Added foo file

---
+++ HW2/foo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# File name: foo.py
+# Author: Jason Esquivel
+# Date created: 9/11/2016
+
+import datetime
+
+# foo(): Gets and print current date and time
+def foo():
+	current_date_time = datetime.datetime.today()
+	print current_date_time
+
+
+foo()
