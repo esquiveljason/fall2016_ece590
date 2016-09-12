2306ce84ecdd55da747bfb77f05eccd6cc0e2303
Jason Esquivel <jesquive@gmu.edu>
2016-09-12 17:53:05 -0400

    finished interrupt.py

--- HW2/interrupt.py
+++ HW2/interrupt.py
@@ -7,9 +7,28 @@
 
 # Interrupt  Real Time
 
-import signal
+import signal, time
 from foo import *
 
+def interruptHandler(signum, stack):
+	global interruptCounter
+	t0 = time.time()
+	foo()
+	t1 = time.time()
+	dt = t1-t0
+	interruptCounter += 1
+	if(interruptCounter % 5 == 0):
+		print "dt = " + str(dt)
 
-def interruptLoop():
+signal.signal(signal.SIGALRM, interruptHandler)
+
+signal.setitimer(signal.ITIMER_REAL, .2, .2)
+
+interruptCounter = 0
+while(interruptCounter < 100):
+	pass
+
+signal.setitimer(signal.ITIMER_REAL, 0)
+#signal.alarm(0)
+
 
