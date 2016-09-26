aff837eb27a9e3bcb345e5915e7ea4a815e67c0b
Jason Esquivel <jesquive@gmu.edu>
2016-09-26 10:25:09 -0400

    initial squat routine

---
+++ HW4/huboFilterChan.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# /* -*-  indent-tabs-mode:t; tab-width: 8; c-basic-offset: 8  -*- */
+# /*
+# Copyright (c) 2013, Daniel M. Lofaro
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the author nor the names of its contributors may
+#       be used to endorse or promote products derived from this software
+#       without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# */
+
+
+
+import hubo_ach as ha
+import ach
+import sys
+import time
+from ctypes import *
+
+# Open Hubo-Ach feed-forward and feed-back (reference and state) channels
+s = ach.Channel(ha.HUBO_CHAN_STATE_NAME)
+f = ach.Channel("huboFilterChan")
+r = ach.Channel(ha.HUBO_CHAN_REF_NAME)
+#s.flush()
+#r.flush()
+
+# feed-forward will now be refered to as "state"
+state = ha.HUBO_STATE()
+
+# feed-back will now be refered to as "ref"
+ref = ha.HUBO_REF()
+refFilter = ha.HUBO_REF()
+print("before while")
+
+while(True):
+	# Get the current feed-forward (state) 
+	[statuss, framesizes] = f.get(refFilter, wait=False, last=True)
+
+	for i in range(ha.HUBO_JOINT_COUNT):
+		ref.ref[i] = refFilter.ref[i]		
+		#ref.ref[i] = doFilter(i, refFilter.ref[i])
+	
+	# Write to the feed-forward channel
+	r.put(ref)
+	#simSleep(T) # create this function (step period of 2 seconds or slower)
+	
+	
+# Close the connection to the channels
+r.close()
+s.close()
+
---
+++ HW4/huboSquat.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# /* -*-  indent-tabs-mode:t; tab-width: 8; c-basic-offset: 8  -*- */
+# /*
+# Copyright (c) 2013, Daniel M. Lofaro
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the author nor the names of its contributors may
+#       be used to endorse or promote products derived from this software
+#       without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# */
+
+
+
+import hubo_ach as ha
+import ach
+import sys
+import time
+from ctypes import *
+
+def crouch(ref, r):
+	ref.ref[ha.RHP] = -.54
+	ref.ref[ha.LHP] = -.54
+	
+	ref.ref[ha.RKN] = .7
+	ref.ref[ha.LKN] = .7
+	
+	ref.ref[ha.RAP] = -.35
+	ref.ref[ha.LAP] = -.35
+
+	# Write to the feed-forward channel
+	r.put(ref)
+	
+	return
+
+def leanRight(ref,r):
+	ref.ref[ha.RHR] = 0.1
+	ref.ref[ha.LHR] = 0.1
+	
+	#ref.ref[ha.RAR] = -.1
+	#ref.ref[ha.LAR] = -.1
+
+	r.put(ref)
+
+def liftLeftLeg(ref,r):
+	ref.ref[ha.LKN] = 1.5
+	
+	r.put(ref)
+
+
+def simSleep(sec, s, state):
+	tick = state.time;
+	dt = 0;
+	while(dt <= sec):
+		s.get(state, wait=False, last=True)
+		dt = state.time - tick;
+	return
+
+
+
+
+# Open Hubo-Ach feed-forward and feed-back (reference and state) channels
+s = ach.Channel(ha.HUBO_CHAN_STATE_NAME)
+#r = ach.Channel("hubo-ref-filter")
+r = ach.Channel(ha.HUBO_CHAN_REF_NAME)
+#s.flush()
+#r.flush()
+
+# feed-forward will now be refered to as "state"
+state = ha.HUBO_STATE()
+
+# feed-back will now be refered to as "ref"
+ref = ha.HUBO_REF()
+
+#while(True):
+	# Get the current feed-forward (state) 
+[statuss, framesizes] = s.get(state, wait=False, last=True)
+	
+leanRight(ref,r)
+print state.time , "Lean right"
+
+simSleep(.5, s, state)
+
+crouch(ref,r)
+print state.time , "Crouch"
+
+simSleep(.5, s, state)
+
+#liftLeftLeg(ref,r)
+
+simSleep(2, s, state)
+
+# Close the connection to the channels
+r.close()
+s.close()
+
+
+
+
+
+	
+
